b42bdb45401c81a5b8edf6c162ae80ed5c5357af
alxjrvs <alxjrvs@gmail.com>
2012-11-18 18:55:36 -0500

    intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wednesdayshelf::Application.config.secret_token = '8b3939b63f5dc924206e382342aedcaec271eece255b3a7653f787e1089d47d34f13eae448809dc6243f198f8c5e961ae7a4006368ed9e9b7aed1e3d15136b71'
