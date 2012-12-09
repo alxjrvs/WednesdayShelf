25f305507cfd88829052287dbd2e66e7d21b30c7
alxjrvs <alxjrvs@gmail.com>
2012-12-08 21:20:28 -0500

    future updates to controller made, beginning s3 integration:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
   def index
     @release = Release.next
+    @release_date = Release.next.ship_date.strftime('%a %d, %b %Y')
+    @next_date = Release.week_after.ship_date.strftime('%a %d, %b %Y')
+    @future_date = Release.two_weeks.ship_date.strftime('%a %d, %b %Y')
+  
     @issues = @release.issues_by_series
   end
 
