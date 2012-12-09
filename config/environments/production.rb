d8124f4b19af131b731e171ce2e76a9cee0bffef
alxjrvs <alxjrvs@gmail.com>
2012-12-09 15:53:26 -0500

    added analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,7 @@
 Wednesdayshelf::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  GA.tracker = "UA-6951085-10"
   # Code is not reloaded between requests
   config.cache_classes = true
 
