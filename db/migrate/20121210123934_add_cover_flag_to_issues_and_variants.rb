181fc9e86ab33e9a4d76253fc88c0d7ae0e31ae4
alxjrvs <alxjrvs@gmail.com>
2012-12-10 08:07:22 -0500

    moving migrations around

--- db/migrate/20121210123934_add_cover_flag_to_issues_and_variants.rb
+++ db/migrate/20121210123934_add_cover_flag_to_issues_and_variants.rb
@@ -1,6 +1,6 @@
 class AddCoverFlagToIssuesAndVariants < ActiveRecord::Migration
   def change
-    add_column :issues, :has_cover?, :boolean
-    add_column :variants, :has_cover?, :boolean
+    add_column :issues, :has_cover, :boolean
+    add_column :variants, :has_cover, :boolean
   end
 end
--- db/migrate/20121210124142_remove_question_mark.rb
+++
@@ -1,8 +0,0 @@
-class RemoveQuestionMark < ActiveRecord::Migration
-  def change
-    remove_column :issues, :has_cover?, :boolean
-    remove_column :variants, :has_cover?, :boolean
-    add_column :issues, :has_cover, :boolean, :default =>false
-    add_column :variants, :has_cover, :boolean, :default => false
-  end
-end
