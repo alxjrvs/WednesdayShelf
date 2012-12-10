e610adbdc293321fcb6b70e103cd8e3d3a5232ac
alxjrvs <alxjrvs@gmail.com>
2012-12-10 13:20:10 -0500

    trying out css change

--- db/migrate/20121210123934_add_cover_flag_to_issues_and_variants.rb
+++ db/migrate/20121210123934_add_cover_flag_to_issues_and_variants.rb
@@ -1,6 +1,6 @@
 class AddCoverFlagToIssuesAndVariants < ActiveRecord::Migration
   def change
     add_column :issues, :has_cover, :boolean, :default => false
-    add_column :variants, :has_cover, :boolean, :default => false
+    add_column :variants, :has_cover, :boolean, :default => false4
   end
 end
