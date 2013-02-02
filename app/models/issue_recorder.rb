e53e2689332ff851e585437d23bf0ec42e12c05b
alxjrvs <alxjrvs@gmail.com>
2013-02-02 08:30:34 -0500

    slight refactor on issue_recorder

--- app/models/issue_recorder.rb
+++ app/models/issue_recorder.rb
@@ -6,21 +6,17 @@ class IssueRecorder
     @release = release
   end
 
-  def has_series?
-    if @listing.main_desc == nil
-      return false
-    else
-      return true
+  def series_publsiher_record
+    unless @listing.main_desc == nil
+      SeriesRecorder.new(@listing, issue).record
+      PublisherRecorder.new(@listing, issue).record
     end
   end
 
   def record
     issue = Issue.where(ListingCreateHasher.new(@listing).digest.merge!(:title => @name)).first_or_create
     @release.issues << issue
-    if has_series?
-      SeriesRecorder.new(@listing, issue).record
-      PublisherRecorder.new(@listing, issue).record
-    end
+    series_publisher_record
   end
 end
 
