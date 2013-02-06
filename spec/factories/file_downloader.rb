323580665b4c5f8b026862e7269785eec09e07f8
alxjrvs <alxjrvs@gmail.com>
2013-02-06 09:26:34 -0500

    fixed file_downloader factory

--- spec/factories/file_downloader.rb
+++ spec/factories/file_downloader.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :file_downloader  do
       ignore do
-        page LoginAgent.new.login.links_with(:text => "Data Files")[0].click
+        page { build(:login_agent).login.links_with(:text => "Data Files")[0].click }
         url "/FileExport/Misc/MasterDataFile-ITEMS.txt"
       end
     initialize_with {FileDownloader.new(page, url)}
