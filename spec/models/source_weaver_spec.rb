0d11fd04edb4ee4719a4242d12112ccc2f91e780
alxjrvs <alxjrvs@gmail.com>
2013-02-10 13:04:13 -0500

    source_weaver specs done

--- spec/models/source_weaver_spec.rb
+++ spec/models/source_weaver_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../support/source_weaver/expectations.rb'
 describe SourceWeaver do
-
-  it "fills_in_a_proper_blurb" do
-
+  before :each do
+    @source_weaver = build(:source_weaver)
   end
+  #it "fills_in_a_proper_blurb" do
+    #unsure
+  #end
   it "digests into hash" do
-
+    @source_weaver.digest.should eq(SW_DIGEST_EXPECTED)
   end
 end
