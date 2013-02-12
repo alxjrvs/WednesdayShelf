273c45d408c29865926a7caa2adcc958eb888729
alxjrvs <alxjrvs@gmail.com>
2013-02-11 23:51:08 -0500

    global constants added

---
+++ config/initializers/global_constants.rb
@@ -0,0 +1,11 @@
+BACKLOG_ARRAY = [
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-ITEMS_201208.txt", "/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201208.TXT"],
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-ITEMS_201209.txt", "/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201209.TXT"],
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-Items_201210.txt","/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201210.TXT"],
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-ITEMS_201211.txt", "/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201211.TXT"],
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-ITEMS_201212.txt","/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201212.TXT"],
+    ["/Downloads/Archives/monthlytools/previews_master_data_file/MasterDataFile-ITEMS_201301.txt","/Downloads/Archives/monthlytools/previews_product_copy/previewsDB_201301.TXT"],
+  ]
+login = LoginAgent.new
+login.login
+MASTER_LOGIN = login
