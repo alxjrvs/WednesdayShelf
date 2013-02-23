be73ba11aa1b47f0e6596298eb0a681541aa92bd
alxjrvs <alxjrvs@gmail.com>
2013-02-23 12:41:58 -0500

    making changes to home controller to accomodate for future home page interaction.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @release = Release.next
     @release_date = @release.ship_date.strftime('%a %d, %b %Y')
+    @home = true
     @issues = @release.issues_by_series
     render 'releases/show'
   end
