ed9c5238496715a8027fdaeedd48e0c4a6d4a632
Alex Jarvis <alxjrvs@gmail.com>
2013-04-03 03:14:13 -0400

    added support for listing create hasher

--- app/models/listing_create_hasher.rb
+++ app/models/listing_create_hasher.rb
@@ -9,19 +9,37 @@ class ListingCreateHasher
     @_create_hash ||= {}
   end
 
+  def title
+    @_title ||= TitleStripper.new(listing.full_title, listing.variant_desc).strip
+  end
+
+  def writer
+    @_writer ||= CreatorNormalizer.new(listing.writer).format
+  end
+
+  def artist
+    @_artist ||= CreatorNormalizer.new(listing.artist).format
+  end
+
+  def cover_artist
+    @_cover_artist ||= CreatorNormalizer.new(listing.cover_artist).format
+  end
+
+  def price
+    @_price ||= listing.price.gsub("S", "").to_f
+  end
+
   def digest
-    create_hash[:title] = TitleStripper.new(listing.full_title, listing.variant_desc).strip
+    create_hash[:title] = title
     create_hash[:description] = listing.full_desc
     create_hash[:diamond_no] = listing.diamd_no
     create_hash[:stock_no] = listing.stock_no
-    create_hash[:writer] = CreatorNormalizer.new(listing.writer).format
-    create_hash[:artist] = CreatorNormalizer.new(listing.artist).format
-    create_hash[:cover_artist] = CreatorNormalizer.new(listing.cover_artist).format
-    #integers
+    create_hash[:writer] = writer
+    create_hash[:artist] = artist
+    create_hash[:cover_artist] = cover_artist
     create_hash[:issue_no] = listing.issue_no.to_i
     create_hash[:max_issue] = listing.max_issue.to_i
-    #float
-    create_hash[:price] = listing.price.gsub("S", "").to_f
+    create_hash[:price] = price
     return create_hash
   end
 end
