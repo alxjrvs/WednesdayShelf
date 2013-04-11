5c520fd56fd67c5314a9adac780572b13d44066b
Alex Jarvis <alxjrvs@gmail.com>
2013-04-11 19:29:37 -0400

    no more initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -86,5 +86,6 @@ module Wednesdayshelf
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
