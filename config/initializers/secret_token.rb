f9fc59de07d5cecf1079d69c4e171edfd70da7ef
Alex Jarvis <alxjrvs@gmail.com>
2013-07-25 00:06:28 -0400

    raize the earth.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WednesdayShelf::Application.config.secret_token = '2fa3b41cb7c5a2dfce8001874c3514112f2103e8fbd84d1a7ecedc20aaa1c50d668698a04827e9c74cd9336dc4a6798bea46b7dd9da13cde1a51bb9195b174df'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WednesdayShelf::Application.config.secret_key_base = '48f5c484cbb047b0c65135dbbcef40e098d9700e699149d40efcae0f69a0c544c10527fc461e12d1c06251ca35f2e270bec75442fbb1e3c5f7c6a3e518a9cb30'
