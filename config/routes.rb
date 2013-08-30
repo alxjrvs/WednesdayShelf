3378d2cfc3a295c1754af146dda632a33cff7314
Alex Jarvis <alxjrvs@gmail.com>
2013-08-30 09:10:42 -0400

    WIP nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ WednesdayShelf::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home#splash'
+   get 'test' => 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
