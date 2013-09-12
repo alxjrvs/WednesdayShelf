c8a0ef7c54c6bf6858f46abbba16900bfef6c747
Alex Jarvis <alxjrvs@gmail.com>
2013-09-11 20:30:09 -0400

    minor css fixes

--- app/assets/javascripts/models/release.js.coffee
+++ app/assets/javascripts/models/release.js.coffee
@@ -1,6 +1,7 @@
 WS.Release = DS.Model.extend
   release_date: DS.attr 'date'
   long_release_date: DS.attr 'string'
+  short_release_date: DS.attr 'string'
   next: DS.attr 'number'
   previous: DS.attr 'number'
   issues: DS.hasMany 'WS.Issue', {embedded: true}
--- app/serializers/release_serializer.rb
+++ app/serializers/release_serializer.rb
@@ -1,5 +1,5 @@
 class ReleaseSerializer < ActiveModel::Serializer
-  attributes :id, :release_date, :next, :previous, :long_release_date
+  attributes :id, :release_date, :next, :previous, :long_release_date, :short_release_date
   embed :ids, include: true
   has_many :issues
 
@@ -9,6 +9,10 @@ class ReleaseSerializer < ActiveModel::Serializer
 
   def previous
     object.previous.try(:id)
+  end
+
+  def short_release_date
+    object.release_date.stamp("12/31/1899")
   end
 
   def long_release_date
